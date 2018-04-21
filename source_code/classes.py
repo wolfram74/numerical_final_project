cbf88e328b59afb89c329c4ca1823741599aaeca
jakeciafre <33564464+jakeciafre@users.noreply.github.com>
2018-04-21 16:07:54 -0600

    1st attempt to fill out add particle

--- source_code/classes.py
+++ source_code/classes.py
@@ -1,9 +1,19 @@
 import numpy
+import random
 
 class Particle():
     def __init__(self):
-        pass
+        self.state=[0,0,0,0]
+    def set_positions(self, x_position, y_position)
+        self.state[:2]=[x_position, y_position]
 
 class System():
-    def __init__(self):
-        pass
+    def __init__(self,width=100,height=100):
+        self.width=width
+        self.height=height
+        self.particle=list[]
+    def add_particle(self):
+        place_x=random.random()*self.width
+        place_y=random.random()*self.height
+        particle_to_place=Particle()
+        particle_to_place.set_position(place_x,place_y)
