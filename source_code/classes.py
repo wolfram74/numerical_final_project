f4eb7b7e4f1b61b8c05cb4791858159d03255a3a
jakeciafre <33564464+jakeciafre@users.noreply.github.com>
2018-04-21 17:59:57 -0600

    Particles now have instance of the system

--- source_code/classes.py
+++ source_code/classes.py
@@ -4,12 +4,16 @@ import random
 class Particle():
     def __init__(self):
         self.state=numpy.zeros(4)
-         
+        self.system=None
+    
     def sepVec(self, vec1, vec2):
         return vec1[:2] - vec2[:2]
  
     def set_positions(self, x_position, y_position):
         self.state[:2]=[x_position, y_position]
+
+    def set_system(self, input_system):
+        self.system=input_system
 
 
 
@@ -20,6 +24,7 @@ class System():
         self.particles=[]
         
     def add_particle(self, particle):
+        particle.set_system(self)
         self.particles.append(particle)
         
     def create_particle(self):
@@ -27,4 +32,5 @@ class System():
         place_y=random.random()*self.height
         particle_to_place=Particle()
         particle_to_place.set_position(place_x,place_y)
+        particle_to_place.set_system(self)
         self.particles.append(particle_to_place)
