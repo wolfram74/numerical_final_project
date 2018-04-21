5773988e37699c34cd66fbe7eb76e3fc40672cf2
jakeciafre <33564464+jakeciafre@users.noreply.github.com>
2018-04-21 17:26:28 -0600

    Made distinction between add_particle and create_particle

--- source_code/classes.py
+++ source_code/classes.py
@@ -6,7 +6,7 @@ class Particle():
         self.state=numpy.zeros(4)
          
     def sepVec(self, vec1, vec2):
-		return vec1[:2] - vec2[:2]
+        return vec1[:2] - vec2[:2]
  
     def set_positions(self, x_position, y_position):
         self.state[:2]=[x_position, y_position]
@@ -17,9 +17,14 @@ class System():
     def __init__(self,width=100,height=100):
         self.width=width
         self.height=height
-        self.particle=[]
-    def add_particle(self):
+        self.particles=[]
+        
+    def add_particle(self, particle):
+        self.particles.append(particle)
+        
+    def create_particle(self):
         place_x=random.random()*self.width
         place_y=random.random()*self.height
         particle_to_place=Particle()
         particle_to_place.set_position(place_x,place_y)
+        self.particles.append(particle_to_place)
