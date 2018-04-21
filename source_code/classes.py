6f094ce03a12f7127b1e634ffe3620c83c5182c5
Eric Davis <ericjdavis232@gmail.com>
2018-04-21 16:34:44 -0600

    Commiting work before pulling from maste git commit -am

--- source_code/classes.py
+++ source_code/classes.py
@@ -3,6 +3,12 @@ import numpy
 class Particle():
     def __init__(self):
         pass
+        
+    def sepVec(self, vec1, vec2):
+		seperation = []
+		seperation = len(vec1)/2.
+		seperation = numpy.subtract(vec1[:2], vec2[:2])
+		return seperation
 
 class System():
     def __init__(self):
