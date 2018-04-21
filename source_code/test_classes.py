4dfa2e416656ac834456ffe11b18bb92b37a0586
wolfram74 <phaugen@gmail.com>
2018-04-21 12:06:47 -0600

    wrote test for adding particles to a system

--- source_code/test_classes.py
+++ source_code/test_classes.py
@@ -17,6 +17,14 @@ class ParticleTest(unittest.TestCase):
         self.assertEqual(2, len(seperation))
         deltax = self.random4vec_1[0] - self.random4vec_2[0]
         self.assertEqual(deltax, seperation[0])
+
+
 class SystemTest(unittest.TestCase):
     def test_meta(self):
         self.assertTrue(True)
+    def test_add_particle(self):
+        system = classes.System()
+        system.add_particle(Particle())
+        self.assertEqual(1, len(system.particles))
+        system.add_particle(Particle())
+        self.assertEqual(2, len(system.particles))
