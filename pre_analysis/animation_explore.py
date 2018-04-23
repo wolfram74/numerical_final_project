694de901b7b7b487bd5e9e134e5086e3eb174013
wolfram74 <phaugen@gmail.com>
2018-04-23 17:15:35 -0600

    made scatter plot animation with time step

--- pre_analysis/animation_explore.py
+++ pre_analysis/animation_explore.py
@@ -1,6 +1,9 @@
+# '''
 import matplotlib.pyplot as pyplot
 import matplotlib.animation as animation
-
+import time
+import math
+import os
 def load_data(file_address):
     file_data = open(file_address, 'r')
     positions = []
@@ -29,16 +32,68 @@ def frame_init():
     axes.set_xlim(0, 1)
     del x_vals[:]
     del y_vals[:]
-    print(field.get_array())
+    print('clearing?')
+    axes.clear()
+    axes.scatter([.4], [.3])
+    # print(field.get_array())
     # field.set_data(x_vals, y_vals)
+
+def frame_gen(frame_data):
+    # del x_vals[:]
+    # del y_vals[:]
+    axes.clear()
+    positions = load_data()
+    x_vals=[math.cos(3.14*frame_num/100.0)]
+    y_vals=[math.sin(3.14*frame_num/100.0)]
+    axes.scatter(x_vals, y_vals)
+    axes.set_ylim(-1, 1)
+    axes.set_xlim(-1, 1)
+
+def find_files(folder):
+    files = os.listdir(folder)
+    usable_files = filter(lambda x: 'ds' not in x, files)
+    address = map(lambda x: "%s/%s" %(folder, x), usable_files)
+    return address
 
 if __name__=='__main__':
     data = load_data('../produced_data/tht_0.6666_w_137/t_0.txt')
-    # print(data)
-    # scatter_plot(data)
-    print(field)
-    print(field.get_array())
-    pyplot.show()
-    frame_init()
-    pyplot.show
+    axes.set_ylim(-1, 1)
+    axes.set_xlim(-1, 1)
+    print(find_files('../produced_data/tht_0.6666_w_137'))
+    # movie = animation.FuncAnimation(
+    #     fig,
+    #     frame_gen,
+    #     frames=find_files('../produced_data/tht_0.6666_w_137')
+    #     )
+    # movie.save('test_circle.mp4')
+# '''
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
+def main():
+    numframes = 100
+    numpoints = 10
+    color_data = np.random.random((numframes, numpoints))
+    x, y, c = np.random.random((3, numpoints))
+
+    fig = plt.figure()
+    scat = plt.scatter(x, y, c=c, s=100)
+    print type(scat)
+    ani = animation.FuncAnimation(fig, update_plot, frames=xrange(numframes),
+                                  fargs=(color_data, scat))
+    plt.show()
+
+def update_plot(i, data, scat):
+    print(len(data), data.shape, i)
+    print(len(data[i]), data[i].shape)
+    if i >0:
+        data[i][0] = data[i-1][0]*.9
+    scat.set_array(data[i])
+    return scat
+
+
+if __name__ == '__main__':
+    main()
+'''
